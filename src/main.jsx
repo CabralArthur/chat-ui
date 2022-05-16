6a112bac2388f202f53c6e7554dd863e8395d0ab
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2022-05-16 13:22:32 -0300

    fix(project-structure): removing react strict mode and inserting messages

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <App />
     <GlobalStyle/>
     <ToastContainer/>
-  </React.StrictMode>
+  </>
 )
